5750c0ad1e6846312cbeff7ee4052b3feade4c81
Brandon Lyons <brandonl@stripe.com>
2020-11-13 22:54:47 -0700

    update model.py and model creation

--- backend/mapping_heat/model.py
+++ backend/mapping_heat/model.py
@@ -2,23 +2,18 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from flask import g
+import pickle
 
 from .db import get_db
 
 cache = {}
 
 def init_model():
-  df = pd.read_sql("SELECT * FROM pitching_data WHERE zone IS NOT NULL", get_db())
-  features = ['player_name', 'pitch_type', 'release_speed', 'zone']
+  filename = '../model/pitch_prob.sav'
+  loaded_model = pickle.load(open(filename, 'rb'))
+  x = pd.read_csv('../model/df_shell.csv')
 
-  x_norm = df[features]
-  x = pd.get_dummies(x_norm)
-  y = df['description'].apply(lambda d: hot_encode_des(d))
-
-  model = LogisticRegression(max_iter=500)
-  model.fit(x, y)
-
-  cache['model'] = model
+  cache['model'] = loaded_model
   cache['x_cols'] = x.columns
 
 def get_model():
@@ -30,12 +25,12 @@ def get_xcols():
   if 'x_cols' not in cache:
     init_model()
   return cache['x_cols']
-
+#
 def predict(params):
   features_df = pd.get_dummies(pd.DataFrame(params))
   features_df = features_df.reindex(columns=get_xcols(), fill_value=0)
   model = get_model()
-  return model.predict(features_df)
+  return model.predict_proba(features_df)
 
 def hot_encode_des(x):
   if x == 'hit_into_play_no_out' or x == 'hit_into_play_score':
